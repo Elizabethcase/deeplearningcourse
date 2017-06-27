b4e0e698408b7b66e5e59f4f1f7f82a2ee7b350f
Ubuntu <ubuntu@ip-10-0-0-11.ec2.internal>
2017-06-27 14:26:12 +0000

    moved png files

--- elizabeth/data/classparse.py
+++ elizabeth/data/classparse.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 from xml.etree import ElementTree
 
-CLASSES = ["n02802426", "container","water","bird bath", "tire", "wheelbarrow", "bucket", "gutter","vegetation"]
+CLASSES = ["n03991062", "n02802426", "container","water","bird bath", "tire", "wheelbarrow", "bucket", "gutter","vegetation","tree","building","sky"]
 
 class XML_preprocessor(object):
 
