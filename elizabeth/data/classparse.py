ec0ccc8c4bb231809cecdfdb0962dd1dfc08aa0b
elizabethcase <elizaebthhcase@gmail.com>
2017-08-05 09:05:24 -0400

    changed class parse to reduce # classes and add noteification if zero bounding boxes

--- elizabeth/data/classparse.py
+++ elizabeth/data/classparse.py
@@ -7,7 +7,10 @@ from xml.etree import ElementTree
 
 #CLASSES = ["n03991062", "n02802426", "container","water","bird bath", "tire", "wheelbarrow", "bucket", "gutter","vegetation","tree","building","sky""]
 
-CLASSES = ["background","pot","clutter","car","trash","woodpile","air conditioning","table","tire","fence","flower pot","porch","house","kayak","front yard","toy","bench","flower bed","driveway","yard","street","pool","chair","tarp","bird bath","container","back yard","gutter","trash bin","fire pit","air conditioner","umbrella","treehouse","basketball","tent","hose","drain","barbecue","awning","fountain","bucket","ladder","toy"]
+#CLASSES = ["background","pot","clutter","car","trash","woodpile","air conditioning","table","tire","fence","flower pot","porch","house","kayak","front yard","toy","bench","flower bed","driveway","yard","street","pool","chair","tarp","bird bath","container","back yard","gutter","trash bin","fire pit","air conditioner","umbrella","treehouse","basketball","tent","hose","drain","barbecue","awning","fountain","bucket","ladder","toy"]
+
+#CLASSES = ["background","pot","clutter","car","trash","table","tire","flower pot","porch","kayak","toy","pool","chair","tarp","bird bath","container","gutter","trash bin","air conditioner","umbrella","basketball","tent","hose","drain","awning","bucket","ladder"]
+
 
 class XML_preprocessor(object):
 
@@ -35,8 +38,10 @@ class XML_preprocessor(object):
                     #print("Res error for file", filename)
                 #if height == 0:
                     #print("Res error for file", filename)
+                n=0 #ssd breaks when training data has zero bounding boxes; this initiattes a count
                 for object_tree in root.findall('object'):
                     for bounding_box in object_tree.iter('bndbox'):
+                        n+=1 #count
                         xmin = float(bounding_box.find('xmin').text)/width
                         ymin = float(bounding_box.find('ymin').text)/height
                         xmax = float(bounding_box.find('xmax').text)/width
@@ -48,9 +53,13 @@ class XML_preprocessor(object):
                         class_name='trash bin'
                     elif class_name=='flower pot':
                         class_name=='pot'
+                    elif class_name=='air conditioning':
+                        class_name=='air conditioner'
                     one_hot_class = self._to_one_hot(class_name)
                     one_hot_classes.append(one_hot_class)
                 image_name = root.find('filename').text
+                if n == 0:
+                    print(image_name) # print image names with zero bounding boxes for removal
                 bounding_boxes = np.asarray(bounding_boxes)
                 one_hot_classes = np.asarray(one_hot_classes)
                 image_data = np.hstack((bounding_boxes, one_hot_classes))
